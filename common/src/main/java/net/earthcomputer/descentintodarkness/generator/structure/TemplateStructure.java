5d847e3b89e7e0af96421a718dbaef97c3a88d98
Joe <burtonjae@hotmail.co.uk>
2024-08-21 18:50:42 +0100

    Allow for separate origins for each template in a template structure

--- common/src/main/java/net/earthcomputer/descentintodarkness/generator/structure/TemplateStructure.java
+++ common/src/main/java/net/earthcomputer/descentintodarkness/generator/structure/TemplateStructure.java
@@ -1,5 +1,6 @@
 package net.earthcomputer.descentintodarkness.generator.structure;
 
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -9,23 +10,21 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.ExtraCodecs;
 
 import java.util.List;
+import java.util.function.Function;
 
 public final class TemplateStructure extends Structure {
     public static final MapCodec<TemplateStructure> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
         StructureProperties.CODEC.forGetter(StructureProperties::new),
-        ExtraCodecs.nonEmptyList(DIDCodecs.singleableList(ResourceLocation.CODEC)).fieldOf("templates").forGetter(structure -> structure.templates),
-        BlockPos.CODEC.optionalFieldOf("origin", BlockPos.ZERO).forGetter(structure -> structure.origin),
+        ExtraCodecs.nonEmptyList(DIDCodecs.singleableList(TemplateEntry.CODEC)).fieldOf("templates").forGetter(structure -> structure.templates),
         Codec.BOOL.optionalFieldOf("ignore_air", true).forGetter(structure -> structure.ignoreAir)
     ).apply(instance, TemplateStructure::new));
 
-    private final List<ResourceLocation> templates;
-    private final BlockPos origin;
+    private final List<TemplateEntry> templates;
     private final boolean ignoreAir;
 
-    private TemplateStructure(StructureProperties props, List<ResourceLocation> templates, BlockPos origin, boolean ignoreAir) {
+    private TemplateStructure(StructureProperties props, List<TemplateEntry> templates, boolean ignoreAir) {
         super(props);
         this.templates = templates;
-        this.origin = origin;
         this.ignoreAir = ignoreAir;
     }
 
@@ -37,5 +36,21 @@ public final class TemplateStructure extends Structure {
     @Override
     public StructureType<?> type() {
         return StructureType.TEMPLATE.get();
+    }
+
+    private record TemplateEntry(
+        ResourceLocation template,
+        BlockPos origin
+    ) {
+        static final Codec<TemplateEntry> CODEC = Codec.either(
+            ResourceLocation.CODEC,
+            RecordCodecBuilder.<TemplateEntry>create(instance -> instance.group(
+                ResourceLocation.CODEC.fieldOf("template").forGetter(TemplateEntry::template),
+                BlockPos.CODEC.optionalFieldOf("origin", BlockPos.ZERO).forGetter(TemplateEntry::origin)
+            ).apply(instance, TemplateEntry::new))
+        ).xmap(
+            either -> either.map(template -> new TemplateEntry(template, BlockPos.ZERO), Function.identity()),
+            entry -> entry.origin.equals(BlockPos.ZERO) ? Either.left(entry.template) : Either.right(entry)
+        );
     }
 }
